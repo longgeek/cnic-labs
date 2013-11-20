d420500733e88aaeea07613cd250a75a19137174
Longgeek <longgeek@gmail.com>
2013-11-20 19:04:15 +0800

    添加组件host选项

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,9 +1,11 @@
 import 'nodes/*'
 
+# '%var%': 表示会自动替换它得值
 
 $command_path                       = '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/bin/bash'
 $source_dir                         = '/opt/stack'
 $source_apt_requires                = ["build-essential", "python-dev", "python-setuptools", "python-pip", "libxml2-dev", "libxslt1-dev", "git", "python-numpy"]
+
 ## MYSQL
 $mysql_host                         = '%mysql%'
 $mysql_root_password                = 'csdb123cnic'
@@ -19,7 +21,7 @@ $service_tenant_name                = 'service'
 $keystone_region                    = 'RegionOne'
 $email_domain                       = 'cnic.cn'
 $keystone_log_verbose               = 'True'
-$keystone_log_debug                 = 'False'
+$keystone_log_debug                 = 'True'
 $keystone_db_user                   = 'keystone'
 $keystone_db_name                   = 'keystone'
 $keystone_db_password               = 'keystone'
@@ -27,15 +29,15 @@ $keystone_logger_level              = 'DEBUG'
 $keystone_logger_handlers           = 'devel,production'
 
 ## GLANCE
+$glance_host                        = '%glance%'
 $glance_db_user                     = 'glance'
 $glance_db_name                     = 'glance'
 $glance_db_password                 = 'glance'
 $glance_source_pack_name            = 'glance.tar.gz'
 $glance_client_source_pack_name     = 'python-glanceclient.tar.gz'
 $glance_log_verbose                 = 'True'
-$glance_log_debug                   = 'False'
+$glance_log_debug                   = 'True'
 $glance_default_store               = 'file'
-$glance_host                        = '%glance%'
 
 ## AMQP
 $rabbit_host                        = '%rabbit%'
@@ -43,6 +45,7 @@ $rabbit_userid                      = 'guest'
 $rabbit_password                    = 'longgeek'
 
 ## CINDER
+$cinder_host                        = '%cinder%'
 $cinder_db_user                     = 'cinder'
 $cinder_db_name                     = 'cinder'
 $cinder_db_password                 = 'cinder'
@@ -50,11 +53,10 @@ $cinder_source_pack_name            = 'cinder.tar.gz'
 $cinder_client_source_pack_name     = 'python-cinderclient.tar.gz'
 $cinder_volume_group                = 'cinder-volumes'
 $cinder_log_verbose                 = 'True'
-$cinder_log_debug                   = 'False'
-$cinder_volume_format               = 'glusterfs'                            # 默认为 'file', 用文件来模拟分区, 设置为 'file'是依赖 '$cinder_volume_size'
-                                                                        # 设置为 'disk'时，依赖 '$cinder_volume_disk_part’
-$cinder_volume_size                 = '5G'                              # 使用 file 的话需要指定大小, 必须有单位
-$cinder_volume_disk_part            = '["sdb1"]'                          # 指定 cinder 使用哪些硬盘分区, 例如: "['sdb1', 'sdc1', 'sdd1']"
+$cinder_log_debug                   = 'True'
+$cinder_volume_format               = 'file'
+$cinder_volume_size                 = '5G'
+$cinder_volume_disk_part            = '["sdc1"]'
 
 
 ## NOVA
@@ -64,11 +66,11 @@ $nova_db_password                   = 'csdb123cnic'
 $nova_source_pack_name              = 'nova.tar.gz'
 $nova_client_source_pack_name       = 'python-novaclient.tar.gz'
 $nova_novnc_source_pack_name        = 'noVNC.tar.gz'
-#$nova_apt_requires                  = ["bridge-utils", "kvm", "libvirt-bin", "libvirt-dev", "python-libvirt", "qemu-kvm", "python-m2crypto", "dnsmasq-utils"]
 $nova_apt_requires                  = ["bridge-utils", "python-m2crypto", "dnsmasq-utils"]
 $nova_log_verbose                   = 'True'
-$nova_log_debug                     = 'False'
+$nova_log_debug                     = 'True'
 $nova_my_ip                         = '%nova%'
+$nova_api_host                      = '%nova_api%'
 $nova_s3_host                       = $nova_my_ip
 $nova_s3_port                       = '3333'
 $nova_metadata_host                 = $nova_my_ip
@@ -87,6 +89,8 @@ $novncproxy_host                    = $nova_my_ip
 $xvpvncproxy_host                   = $nova_my_ip
 $vncserver_proxyclient_address      = $nova_my_ip
 $qemu_version                       = '1.5.3'
+$nova_control_network               = 'True'
+$nova_control_compute               = 'True'
 
 ## HORIZON
 $horizon_apt_requires               = ["apache2", "memcached", "python-memcache", "nodejs", "libapache2-mod-wsgi"]
@@ -94,12 +98,14 @@ $horizon_source_pack_name           = 'horizon.tar.gz'
 $horizon_db_user                    = 'horizon'
 $horizon_db_name                    = 'horizon'
 $horizon_db_password                = 'horizon'
+$memcache_host                      = '%memcache%'
 $savanna_host                       = 'NULL'
 
 ## MONITOR
 $monitor_db_name                    = 'monitor'
 $monitor_db_user                    = 'monitor'
 $monitor_db_password                = 'monitor'
+$ceilometer_api_host                = '%ceilometer_host%'
 
 ## SWIFT
 $swift_proxy_host                   = $nova_my_ip
@@ -109,4 +115,4 @@ $gluster_swift_version              = $swift_version
 
 ## GLUSTERFS
 $glusterfs_version                  = '3.4.0'
-$glusterfs_nodes_list               = '172.16.0.201 172.16.0.202'
+$glusterfs_nodes_list               = '%glusterfs_nodes_list%'
