3402f0b8ba78df8eac49052f6d5774a9995f08d3
Longgeek <longgeek@gmail.com>
2013-11-20 19:05:02 +0800

    delete mongodb service

--- puppet/modules/ceilometer-client/manifests/service.pp
+++ puppet/modules/ceilometer-client/manifests/service.pp
@@ -1,13 +1,4 @@
 class ceilometer-client::service {
-    Service["mongodb"] -> Service["ceilometer-agent-compute"]
-
-    service { "mongodb":
-        ensure => "running",
-        enable => true,
-        hasstatus => true,
-        hasrestart => true,
-    }
-
     service { "ceilometer-agent-compute":
         ensure => "running",
         enable => true,
