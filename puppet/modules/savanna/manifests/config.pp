c56c2da0208f8f1786bf5c638cd07561df5b34af
Longgeek <longgeek@gmail.com>
2013-11-20 08:52:35 +0800

    Add savanna init script & log file.

--- puppet/modules/savanna/manifests/config.pp
+++ puppet/modules/savanna/manifests/config.pp
@@ -1,12 +1,36 @@
 class savanna::config {
-    file { "/etc/savanna":
-        ensure => directory,
-        notify => File["/etc/savanna/savanna.conf"],
+
+    user { "savanna":
+        ensure => "present",
+        shell => "/usr/sbin/nologin",
+        notify => File["/etc/savanna"],
     }
 
-    file { "/etc/savanna/savanna.conf":
-        content => template("savanna/savanna.conf.erb"),
-        notify => Class["savanna::service"],
+    file { 
+        "/etc/savanna":
+            ensure => directory,
+            owner => 'savanna';
+        "/var/log/savanna/":
+            ensure => directory,
+            owner => 'savanna',
+            notify => Exec["savanna upstart"],
+    }
+
+    exec { "savanna upstart":
+        command => "ln -s /lib/init/upstart-job /etc/init.d/savanna-api",
+        path => $command_path,
+        unless => "ls /etc/init.d/savanna-api",
+        notify => File["/etc/init/savanna-api.conf"],
+    }
+
+    file { "/etc/init/savanna-api.conf":
+        content => template("savanna/savanna-api.conf.erb"),
+        notify => File["/etc/savanna/savanna.conf"],
     }
    
+    file { "/etc/savanna/savanna.conf":
+        content => template("savanna/savanna.conf.erb"),
+        owner => 'savanna',
+        notify => Class["savanna::service"],
+    }
 }
--- puppet/modules/savanna/manifests/service.pp
+++ puppet/modules/savanna/manifests/service.pp
@@ -1,8 +1,8 @@
 class savanna::service {
-    exec { "start savanna-api":
-        command => "nohup $source_dir/savanna/savanna-venv/bin/python $source_dir/savanna/savanna-venv/bin/savanna-api --config-file /etc/savanna/savanna.conf > /dev/null 2>&1 &",
-        path => $command_path,
-        unless => "netstat -ltunp | grep 8386",
-        require => Class["savanna::install"],
+    service { "savanna-api":
+        ensure => 'running',
+        enable => true,
+        hasstatus => true,
+        hasrestart => true,
     }
 }
