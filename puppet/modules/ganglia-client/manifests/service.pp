433492ac3b4918d0c95820b4db6d732a97169123
Longgeek <longgeek@gmail.com>
2013-11-21 14:19:45 +0800

    监测 ganglia-server 端口

--- puppet/modules/ganglia-client/manifests/service.pp
+++ puppet/modules/ganglia-client/manifests/service.pp
@@ -4,5 +4,12 @@ class ganglia-client::service {
         path => $command_path,
         refreshonly => true,
         require => Class["ganglia-client::config"],
+        notify => Exec["listen ganglia-server port"],
+    }
+
+    exec { "listen ganglia-server port":
+        command => "/etc/init.d/ganglia-monitor restart",
+        path => $command_path,
+        unless => "[ \"`telnet $memcache_host 8649 | wc -l`\" -ne '1' ]",
     }
 }
