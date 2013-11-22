f47f7cd9b99fda03f70b25420eef7685dacd905a
Longgeek <longgeek@gmail.com>
2013-11-22 11:05:48 +0800

    增加 ganglia-server 和 gmetad 服务状态监测

--- puppet/modules/ganglia/manifests/service.pp
+++ puppet/modules/ganglia/manifests/service.pp
@@ -5,5 +5,19 @@ class ganglia::service {
         path => $command_path,
         refreshonly => true,
         require => Class["ganglia::config"],
+        notify => Exec["gmetad status"],
+    }
+
+    exec { "gmetad status":
+        command => "/etc/init.d/gmetad restart",
+        path => $command_path,
+        unless => "ps aux | grep -v grep | grep gmetad",
+        notify => Exec["ganglia-monitor status"],
+    }
+
+    exec { "ganglia-monitor status":
+        command => "/etc/init.d/ganglia-monitor restart",
+        path => $command_path,
+        unless => "ps aux | grep -v grep | grep gmond",
     }
 }
