06e68158b86874fb55e70cf710226934f5d8600e
Longgeek <longgeek@gmail.com>
2013-11-26 16:16:36 +0800

    路径修改

--- install_cobbler_puppet.sh
+++ install_cobbler_puppet.sh
@@ -254,8 +254,8 @@ cobbler sync; /etc/init.d/cobbler restart
 apt-get -y --force-yes install nginx php5-fpm php5-sqlite
 cp $TOP_DIR/eccp-web/default /etc/nginx/sites-available/
 rm -fr /usr/share/nginx/www/index.html
-cp -r  $TOP_DIR/eccp-web/www /usr/share/nginx/www/
-cp www.conf /etc/php5/fpm/pool.d/
+cp -r  $TOP_DIR/eccp-web/www/* /usr/share/nginx/www/
+cp $TOP_DIR/eccp-web/www.conf /etc/php5/fpm/pool.d/
 /etc/init.d/php5-fpm restart
 /etc/init.d/nginx restart
 
