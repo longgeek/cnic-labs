439335e4033e42fd298a8f93b5e3168a39c5a676
Longgeek <longgeek@gmail.com>
2013-11-27 10:51:57 +0800

    mv swift-glusterfs

--- puppet/modules/swift/manifests/init.pp
+++
@@ -1,4 +0,0 @@
-class swift {
-    Class["swift"] -> Class["swift::swift-install"] -> Class["swift::gluster-swift-install"] -> Class["swift::config"] -> Class["swift::service"]
-    include swift, swift-install, gluster-swift-install, config, service
-}
